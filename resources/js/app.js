c8f7171f5b7b5e4cae628fa77b048cb47030ea8a
peter-28 <peterlase28@gmail.com>
2025-03-13 09:23:54 +0700

    update

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,9 @@
 import "./bootstrap";
-// import "@coreui/coreui/dist/js/coreui.bundle.min.js";
+import "@coreui/coreui/dist/js/coreui.bundle.min.js";
 import Swal from "sweetalert2";
 
 // // or via CommonJS
 const Swal = require("sweetalert2");
-// $(function () {
-//     $('[data-toggle="tooltip"]').tooltip();
-// });
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+});
